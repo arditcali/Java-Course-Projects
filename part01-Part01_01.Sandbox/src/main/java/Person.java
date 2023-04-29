0c24890e259010a865a188e2dc9a8907b5dc586d
arditcali <129833461+arditcali@users.noreply.github.com>
2023-04-29 17:57:44 +0200

    Add files via upload

---
+++ part01-Part01_01.Sandbox/src/main/java/Person.java
@@ -0,0 +1,15 @@
+
+public class Person {
+    
+    private String name;
+    private String ethnicity;
+    private int age;
+    
+    public Person(String name, String race){
+        
+        this.name = name;
+        this.ethnicity = race;
+        this.age = 25;
+        
+    }
+}
---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        
+        Person person = new Person("Ben", "White");
+        
+        System.out.println(person);
+    }
+}    
+    
+
+
